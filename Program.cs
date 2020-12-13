1693bf67cd71bdb924344f278bfc91d13c7c849c
Lawrence <darkflare1055@gmail.com>
2020-12-13 18:13:10 +0800

    feat: add list mode

--- Program.cs
+++ Program.cs
@@ -9,6 +9,10 @@ namespace MarkdownToMindmap
     {
         static StringBuilder _puml = new StringBuilder();
         static string _headingPattern = @"#+ .*\n";
+        static string _listPattern = @"(-|\*|\+) .*\n";
+        static string _pattern = @$"{_headingPattern}|{_listPattern}";
+        static bool _listMode = false;
+        static int _headingLevel = 1;
 
         static void MarkdownToMindmap(string dir)
         {
@@ -16,50 +20,67 @@ namespace MarkdownToMindmap
             int headingCount = 0;
             int line = 0;
             bool isFliped = false;
+            Regex subheadingRegex = new Regex(@"^## ");
+            Regex listRegex = new Regex(@"^(-|\*|\+) ");
+            Regex hashToAsteriskRegex = new Regex("#");
 
             using (StreamReader reader = new StreamReader(dir))
             {
                 markdownContent = reader.ReadToEnd();
             }
 
-            MatchCollection headings = Regex.Matches(markdownContent, _headingPattern);
+            string pattern = _listMode? _pattern : _headingPattern;
+            MatchCollection headings = Regex.Matches(markdownContent, pattern);
             headingCount = headings.Count;
 
             _puml.Append("@startmindmap\n");
             foreach (Match heading in headings)
             {
                 line++;
-                Regex subheadingRegex = new Regex(@"^## ");
-                if (!isFliped && subheadingRegex.IsMatch(heading.ToString()) && line > headingCount / 2)
+                string header = heading.ToString();
+                if (!isFliped && subheadingRegex.IsMatch(header) && line > headingCount / 2)
                 {
                     _puml.Append("left side\n");
                     isFliped = true;
                 }
-                Regex hashToAsteriskRegex = new Regex("#");
-                _puml.Append(hashToAsteriskRegex.Replace(heading.ToString(), @"*"));
+
+                if(_listMode)
+                {
+                    CheckHeadingLevel(heading.ToString());
+                }
+
+                if(_listMode && listRegex.IsMatch(header))
+                    _puml.Append(listRegex.Replace(header, new String('*', _headingLevel+1) + "_ "));
+                else
+                    _puml.Append(hashToAsteriskRegex.Replace(header, @"*"));
             }
             _puml.Append("@endmindmap");
+        }
+
+        static void CheckHeadingLevel(string heading)
+        {
+            MatchCollection hashs = Regex.Matches(heading, @"#");
+            if(hashs.Count != 0) _headingLevel = hashs.Count;
         }
 
         public static void Main(string[] args)
         {
             Console.WriteLine("Please input the absolute path of markdown files:");
             string targetDir = Console.ReadLine();
+            Console.WriteLine("Use list mode? (Y|else no)");
+            if(Console.ReadLine() == "Y")
+                _listMode = true;
             
             if(targetDir.EndsWith(".md") ? true : false)
             {
                 FileInfo file = new FileInfo(targetDir);
                 Directory.SetCurrentDirectory(file.DirectoryName);
+                string output = Regex.Match(file.Name, @".*?(?=\.md)").ToString();
 
                 MarkdownToMindmap(targetDir);
 
-                Console.WriteLine("Please input the output name:");
-                string output = Console.ReadLine();
-
                 using (StreamWriter writer = new StreamWriter($"{output}.puml"))
-                {
                     writer.Write(_puml.ToString());
-                }
             }
             else
             {
---
+++ README.md
@@ -0,0 +1,17 @@
+# MarkdownToMindmap
+
+## Introduce
+
+将Makrdown的标题转化为PlantUML中的MindMap格式
+
+### List Mode
+
+将含有标题和一级List的Markdown转化为MindMap格式
+
+## 注意事项
+
+1.  标题只能使用strict模式的#标题
+2.  List可以使用-、*、+
+3.  List只能为一级List
+4.  多级List不会分级
+5.  输入md文件的绝对地址
